e4bd1eef179b5dafa06d0aca5f9f98d89489ef3f
Rizoan Hossain Rishad <95619717+RizoanHossainRishad@users.noreply.github.com>
2024-12-30 23:40:27 +0600

    Update 2048A Kevin and combination lock.cpp

--- 2048A Kevin and combination lock.cpp
+++ 2048A Kevin and combination lock.cpp
@@ -26,22 +26,25 @@ int main(){
     while(t--){
         ll x;
         cin >> x;
-        string str = to_string(x);
+        string str = to_string(x); // Ekta string nibo str jate x er int take string e convert kore ene rakhbo
 
         // Check and remove all occurrences of "33"
-        size_t pos = str.find("33");
-        while (x >= 33) {
+        size_t pos = str.find("33"); // check korbo je str e kono 33 ase kina 
+        while (x >= 33) { // loop cholbe until x , 33 er cheye choto hoye jay
             if (pos != string::npos) { // string::npos is largest possible value of size_t type so as long as pos isnt that the codition will hold true
-              //When you use functions like find, rfind, find_first_of, etc., they return string::npos if the substring or character is not found.
+              //When you use functions like find, rfind, find_first_of, etc., they return string::npos if the substring or character is not found. jodi 33 pai tahole pos e index of str jekhane 33 paise oita store hobe jemon 133 hole
+                // pos = 1 hobe karon 1th index theke consecutive 33 shuru hoise . 
                 while (pos != string::npos) {
-                    str.erase(pos, 2);
-                    pos = str.find("33");
+                    str.erase(pos, 2); //pos e 1 pele 1 soho 2nd index o delete korte hobe so eijonne erase(pos,2) kora hoise 
+                    pos = str.find("33");//ar kono 33 ase kina ta check hocche
                 }
-                if (!str.empty()) {
-                    x = stoll(str);
+                if (!str.empty()) { //33 remove korar pore jodi str empty na hoy means je x ekhono 0 hoy nay so ei string take long long e niye x e rakhte hobe
+                    x = stoll(str); // stoll hocche string to long long , erokom aro datatype er function gula holo
+                    // stod (string to double), stof(string to float), stoi(string to int) , stol(string to long) , stoul(string to unsigned int)
+                    //, stoull(string to unsigned long long), stold( string to long double) etc
                 } else {
-                    x = 0;
-                    break;
+                    x = 0; //jodi str empty hoye jay means je x o 0 hoye gese which is my ideal goal for "YES"
+                    break;// braeking out of the loop
                 }
                 pos = str.find("33");
             } else {
