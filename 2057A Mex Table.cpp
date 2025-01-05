0d195981fb0813895693f153c8ec021e7d5c4d2f
Rizoan Hossain Rishad <95619717+RizoanHossainRishad@users.noreply.github.com>
2025-01-06 00:04:29 +0600

    2057A Mex Table

---
+++ 2057A Mex Table.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n, m;
+        cin >> n >> m;
+      
+       
+      int m2=max(n,m);
+      cout<<m2+1<<endl;
+   
+ }
+  
+   
+}
+
+
